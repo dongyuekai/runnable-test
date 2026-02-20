96668a644f00f67e4428dd9e32233f44fabb6a8b
dongyuekai <457545648@qq.com>
2026-02-20 12:04:38 +0800

    feat: 扩展属性，用 RunnablePassthrough.assign，就像 Object.assign 一样

--- src/RunnablePassthrough.mjs
+++ src/RunnablePassthrough.mjs
@@ -2,17 +2,30 @@ import 'dotenv/config';
 import { RunnablePassthrough, RunnableLambda, RunnableSequence, RunnableMap } from "@langchain/core/runnables";
 
 // 创建一个简单的 RunnableLambda
+// const chain = RunnableSequence.from([
+//   RunnableLambda.from((input) => ({ concept: input })),
+//   RunnableMap.from({
+//     original: new RunnablePassthrough(),
+//     processed: RunnableLambda.from((obj) => ({
+//       concept: obj.concept,
+//       upper: obj.concept.toUpperCase(),
+//       length: obj.concept.length,
+//     }))
+//   })
+// ]);
+
 const chain = RunnableSequence.from([
-  RunnableLambda.from((input) => ({ concept: input })),
-  RunnableMap.from({
+  (input) => ({ concept: input }),
+  RunnablePassthrough.assign({
     original: new RunnablePassthrough(),
-    processed: RunnableLambda.from((obj) => ({
-      concept: obj.concept,
+    processed: (obj) => ({
+      concept: input,
       upper: obj.concept.toUpperCase(),
       length: obj.concept.length,
-    }))
+    })
   })
 ]);
+
 const input = "hello world";
 const result = await chain.invoke(input);
 console.log(result);
